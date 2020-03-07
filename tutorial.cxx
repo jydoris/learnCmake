dc759d38d009586fe41d0ddb63d63997d62a9bef
doris <yljdoris@foxmail.com>
2020-03-07 10:44:41 +0800

    step2: addlibrary optianlly

--- tutorial.cxx
+++ tutorial.cxx
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <string>
 #include "TutorialConfig.h"
+#ifdef USE_MYMATH
 #include "MathFunctions.h"
-
-int main(int argc, char* argv[])
+#endif
+int main(int argc, char *argv[])
 {
-   if (argc < 2) {
+  if (argc < 2)
+  {
     // report version
     std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
               << Tutorial_VERSION_MINOR << std::endl;
@@ -17,10 +19,14 @@ int main(int argc, char* argv[])
   }
 
   // convert input to double
-   const double inputValue = std::stod(argv[1]);
+  const double inputValue = std::stod(argv[1]);
 
-  // calculate square root
+// calculate square root
+#ifdef USE_MYMATH
   const double outputValue = mysqrt(inputValue);
+#else
+  const double outputValue = sqrt(inputValue);
+#endif
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
